c13ff42a0b186383776f763a8aa5036a54414e2b
@nrouyer38 <n.rouyer38@gmail.com>
2022-01-24 19:10:39 +0100

    add comments

--- src/main/java/tsi/ensg/projet/services/ParticipantService.java
+++ src/main/java/tsi/ensg/projet/services/ParticipantService.java
@@ -14,22 +14,43 @@ public class ParticipantService {
     @Autowired
     private ParticipantRepo repository;
 
+    /**
+     * find All Participant in repo
+     * @return List of all Participant
+     */
     public List<Participant> findAll() {
         List<Participant> participant = (List<Participant>) repository.findAll();
         return participant;
     }
 
+    /**
+     * save an Participant in repo
+     * @param participant
+     */
     public void save(Participant participant) {  // pas testé avec persist !
         repository.save(participant);
     }
 
+    /**
+     * find Participant by his ID
+     * @param id
+     * @return an Participant
+     */
     public Optional<Participant> findById(Long id) {
         return repository.findById(id);
     }
 
+    /**
+     * delete a Participant
+     * @param participant
+     */
     public void delete(Participant participant) {
         repository.delete(participant);
     }
 
+    /**
+     * delete a Participant by his ID
+     * @param id
+     */
     public void deleteById(Long id) {repository.deleteById(id);}
 }
